7094d48f79d18cd612a68e3fb7c3a3a610655d20
Yanping Liu <yanpliu@thoughtworks.com>
2018-10-31 20:14:21 +0800

    init empty project

---
+++ src/test/java/refactoring/ExampleTest.java
@@ -0,0 +1,12 @@
+package refactoring;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ExampleTest {
+    @Test
+    void should_be_true() {
+        assertTrue(true);
+    }
+}
